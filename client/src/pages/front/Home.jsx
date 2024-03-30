e2dafcf08f47c69261f6bb1eabaa460985cd5810
adnan4k <fayomuhe5@gmail.com>
2024-03-30 10:35:53 +0300

    first phase of featured post done

--- client/src/pages/front/Home.jsx
+++ client/src/pages/front/Home.jsx
@@ -7,6 +7,12 @@ function Home() {
     <div>
         <Navbar />
         <Hero />
+        <section className='featured flex flex-col justify-center items-center mt-5'>
+             <h2 className='text-center text-5xl font-extrabold'>Feature post</h2>
+             <p className='text-2xl my-5'>I want to talk about the hard stuff people wonder about but maybe are embarassed...</p>
+             <p className='text-2xl my-'>I want to talk about the hard stuff are embarassed...</p>
+        </section>
+
     </div>
   )
 }
