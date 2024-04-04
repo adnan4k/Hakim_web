e841b3613faa5346b2bbb892de4db282b9be59e0
adnan4k <fayomuhe5@gmail.com>
2024-04-04 06:41:21 +0300

    slider added for latest post

--- client/src/components/PostSlider.jsx
+++ client/src/components/PostSlider.jsx
@@ -1,49 +1,120 @@
+
 import React from "react";
+import axios from "axios";
+import { useState,useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import PreviousBtn from "./PreviousBtn";
+import NextBtn from "./NextBtn";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIos from "@mui/icons-material/ArrowBackIos";
 
-function MultipleItems() {
+function Testimony() {
+  // const [data,setData] = useState([]);
+  // useEffect(() => {
+  //   // Function to fetch data from the API
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get('https://groom-health-care.onrender.com/testimony/get-testimony');
+  //       console.log(response.data[0].image);
+  //       setData(response.data); // Assuming the response data is the array of testimonies
+  //     } catch (error) {
+  //       console.error("Error fetching testimonies:", error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, [])
+
+  const data = [
+    {
+      title:"Surguery in addis abeba",
+      description:"We love useful stuff, and we love quality writing, that's why we send out an editorial email newslet .........................",
+      image:"/images/image1.png"
+
+    },
+    {
+      title:"Surguery in addis abeba",
+      description:"We love useful stuff, and we love quality writing, that's why we send out an editorial email newslet .........................",
+      image:"/images/image1.png"
+
+    },
+    {
+      title:"Surguery in addis abeba",
+      description:"We love useful stuff, and we love quality writing, that's why we send out an editorial email newslet .........................",
+      image:"/images/image3.png"
+
+    },
+    {
+      title:"Surguery in addis abeba",
+      description:"We love useful stuff, and we love quality writing, that's why we send out an editorial email newslet .........................",
+      image:"/images/image2.png"
+
+    },
+  ]
   const settings = {
     dots: true,
+    fade: false,
+    autoplay: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3
+    speed: 800,
+    autoplaySpeed: 6000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <NextBtn icon={ArrowForwardIosIcon} />,
+    prevArrow: <PreviousBtn icon={ArrowBackIos} />,
+    endDots: (dots) => <ul>{dots}</ul>,
+    customPaging: (i) => (
+      <div className="ft-slick__dots--custom">
+        <div className="loading" />
+      </div>
+    ),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
+  
   return (
-    <div className="slider-container">
-      <Slider {...settings}>
-        <div>
-          <h3 className="bg-green-500">1</h3>
-        </div>
-        <div>
-          <h3 className="bg-green-500">2</h3>
-        </div>
-        <div>
-          <h3 className="bg-green-500">3</h3>
-        </div>
-        <div>
-          <h3 className="bg-green-500">4</h3>
-        </div>
-        <div>
-          <h3 className="bg-green-500">5</h3>
-        </div>
-        <div>
-          <h3 className="bg-green-500">6</h3>
-        </div>
-        <div>
-          <h3 className="bg-green-500">7</h3>
-        </div>
-        <div>
-          <h3 className="bg-green-500">8</h3>
-        </div>
-        <div>
-          <h3 className="bg-green-500">9</h3>
-        </div>
-      </Slider>
+    <div className="testimonial  mb-[3%] ">
+      <div className="w-[90%] ml-[5%] items-center justify-center py-10 mx-2 sm:mx-14">
+        <Slider {...settings}>{data.map((item) => (
+          <div className="p-4 w grid grid-cols-1 sm:grid sm:grid-cols-3  ">
+            <div className=" bg-white flex flex-col justify-center items-center p-5 rounded-lg shadow-lg">
+            <div className="flex flex-col">
+            <img src={item.image} alt={item.title} className="mx-auto w-full rounded-2xl " />
+            <h2 className="text-center text-xl font-semibold ">{item.title}</h2>
+            </div>
+            <p className="text-center my-5">{item.description}</p>
+             <button className="px-10 py-2 bg-[#41BAFF] text-white text-[16px]  text-nowrap rounded-3xl">Read More</button>
+            </div>
+          </div>
+      ))}</Slider>
+      </div>
     </div>
   );
 }
-
-export default MultipleItems;
+export default Testimony;
