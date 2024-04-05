7dae5292356160b7f034ac12dd496f9878cb992e
adnan4k <fayomuhe5@gmail.com>
2024-04-05 16:44:59 +0300

    width adjusted

--- client/src/components/Advertisment.jsx
+++ client/src/components/Advertisment.jsx
@@ -7,7 +7,7 @@ function Advertisment() {
       <div class="bg-[#16ABFF] bg-cover bg-center h-32 flex items-center justify-center">
         <h2 className="text-3xl text-white my-10 font-bold">Discover Latest advertisment</h2>
       </div>
-      <div className="slider container">
+      <div className="slider ">
         <AdsSlider />
       </div>
     </div>
