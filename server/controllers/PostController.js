f7f7386003705f016b276e3211d734280d0eb0ec
adnan4k <fayomuhe5@gmail.com>
2024-04-09 21:50:44 +0300

    post controller for crud added

---
+++ server/controllers/PostController.js
@@ -0,0 +1,82 @@
+import Post from "../models/Post.js";
+
+
+export const addPost = async(req,res,next) =>{
+        const { title, content } = req.body;
+        try {
+            const updateData = {
+                title: title,
+                content: content,
+            };
+    
+            if (req.file) {
+                updateData.image = req.file.filename; 
+            }
+    
+            const savedPost = await Post.create(updateData);
+    
+            return res.status(201).json({ savedPost });
+    
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ message: "An error occurred" });
+        }
+    }
+ 
+    export const updatePost = async (req, res) => {
+        const { title, content } = req.body;
+        const { id } = req.params;
+    
+        try {
+            const updateData = {
+                title: title,
+                content: content,
+            };
+    
+            if (req.file) {
+                updateData.image = req.file.filename;
+            }
+    
+            const [updated] = await Post.update(updateData, {
+                where: { id: id }
+            });
+    
+            if (updated) {
+                // const updatedPost = await Post.findByPk(id);
+                return res.status(200).json({ message: 'Post updated successfully', Post: updatedPost });
+            } else {
+                return res.status(404).json({ message: 'Post not found' });
+            }
+    
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json(error);
+        }
+    }
+    export const allPost = async (req, res) => {
+        // console.log('here is Post')
+        try {
+        const post = await Post.findAll();
+            return res.status(200).json(post);
+        } catch (error) {
+            return res.status(500).json({ message: "Server error" });
+        }
+    }
+
+    export const deletePost = async (req, res) => {
+        const { id } = req.params;
+        try {
+            const deleted = await Post.destroy({
+                where: { id: id }
+            });
+    
+            if (deleted) {
+                return res.status(200).json({ message: "Post deleted" });
+            } else {
+                return res.status(404).json({ message: "Post doesn't exist" });
+            }
+    
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }
