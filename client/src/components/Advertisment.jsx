e5b867af41fe139fbef0fe3c7082e5f25f0a65b0
adnan4k <fayomuhe5@gmail.com>
2024-04-09 15:03:13 +0300

    wrap up

--- client/src/components/Advertisment.jsx
+++ client/src/components/Advertisment.jsx
@@ -5,9 +5,9 @@ function Advertisment() {
   return (
     <div>
       <div className="bg-[#16ABFF] h-64">
-        <div className="h-32 flex items-center justify-center bg-[url('/images/real.png')] bg-no-repeat bg-left-top">
+        <div className="h-32 mt- flex items-center justify-center bg-[url('/images/ac2.png')] bg-no-repeat bg-left-top">
           <div>
-            <div className="bg-[url('/images/real.png')] bg-no-repeat bg-left-bottom">
+            <div className="bg-[url('/images/ac.png')] bg-no-repeat bg-right">
               <h2 className="text-3xl text-white my-10 font-bold">Discover Latest advertisment</h2>
             </div>
           </div>
