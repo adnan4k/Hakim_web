d5b9ea5784ab7e6a1b6e86b9824a11b7d40d1dd3
adnan4k <fayomuhe5@gmail.com>
2024-04-10 10:45:54 +0300

    create post added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/front/Home';
 import CreatePost from './pages/dashboard/CreatePost';
+import CreateAds from './pages/dashboard/CreateAds';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/create-ads" element={<CreateAds />} />
         </Routes>
       </BrowserRouter>
     </div>
