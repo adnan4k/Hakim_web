ca39583c960acab6ea12ed4773c4b73f0da90232
adnan4k <fayomuhe5@gmail.com>
2024-04-10 05:06:23 +0300

    ads route added

---
+++ server/routes/AdsRoute.js
@@ -0,0 +1,12 @@
+;
+import { addAds, allAds, deleteAds, updateAds } from "../controllers/AdsController.js";
+import express from "express"
+
+const adsRouter = express.Router();
+
+adsRouter.ads('/create-ads',addAds);
+adsRouter.put('/update-ads/:id',updateAds),
+adsRouter.delete('/delete-ads/:id',deleteAds),
+adsRouter.get('/get-ads',allAds)
+
+export default adsRouter
